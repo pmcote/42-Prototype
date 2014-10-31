5203225ba0b244e9bb70c6eac446c277312b4388
pmcote <pmcote1@gmail.com>
2014-10-31 11:52:35 -0400

    new init

--- server.js
+++ server.js
@@ -1,17 +1,64 @@
-    var express  = require('express');
-    var app      = express();                               // create our app w/ express
-    // var mongoose = require('mongoose');                     // mongoose for mongodb
-    var morgan = require('morgan');             // log requests to the console (express4)
-    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+// Packages
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
 
-    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-    app.use(morgan('dev'));                                         // log every request to the console
-    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-    app.use(bodyParser.json());                                     // parse application/json
-    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-    app.use(methodOverride());
+// Mongoose instance and connection to our mongolab database
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://savethe:children@ds049150.mongolab.com:49150/information');
 
-    // listen (start app with node server.js)
-    app.listen(8080);
-    console.log("App listening on port 8080");
+// Models, where we pull in the models for mongoDB data
+var Test = require('./models/test');
+
+// bodyParser, whoot
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000; // set our port
+
+// Routing
+var router = express.Router(); // get an instance of the express Router
+
+// Middleware that happens each time we make a request
+router.use(function(req, res, next) {
+	console.log('someone made a request, was it you?');
+	next(); // move to the next associated middleware
+})
+
+// Test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });	
+})
+
+// API Routes
+router.route('/tests')
+	// create a test at POST https://localhost:300/api/test
+	.post(function (req, res){
+
+		var test = new Test(); // create a new instance of test
+		test.name = req.body.name; // what is this test's name in the request?
+
+		test.save(function (err) { // yey 4 err hndlng doh
+			if (err) res.send(err);
+			res.json({message: 'Test created'});
+		});
+
+	})
+
+	.get(function (req, res) {
+		Test.find(function (err, tests) {
+			if (err) res.send(err);
+			res.json(tests)
+		})
+	})
+
+
+
+// Register Routes
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// Server
+app.listen(port);
+console.log('Server listening on', port);
