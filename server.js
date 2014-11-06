cd9b3bb9acfb7bb30b497bc158113c9ac9d57c1f
pmcote <pmcote1@gmail.com>
2014-11-06 16:45:21 -0500

    minimzing api, ready to receive basic templates

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var Test = require('./models/test');
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.set('views', __dirname + '/public');
+app.set('view engine', 'jade');
 
 var port = process.env.PORT || 3000; // set our port
 
@@ -28,72 +30,9 @@ router.use(function(req, res, next) {
 
 // Test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
+	res.json({ message: 'hooray! welcome to our api!' });
+	res.render('title');	
 })
-
-// API Routes
-router.route('/tests')
-	// create a test at POST https://localhost:300/api/test
-	.post(function (req, res){
-
-		var test = new Test(); // create a new instance of test
-		test.name = req.body.name; // what is this test's name in the request?
-
-		test.save(function (err) { // yey 4 err hndlng doh
-			if (err) res.send(err);
-			res.json({message: 'Test created'});
-		});
-	})
-
-	.get(function (req, res) {
-		Test.find(function (err, tests) {
-			if (err) res.send(err);
-			res.json(tests)
-		})
-	});
-
-router.route('/bears/:bear_id')
-
-	// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
-	.get(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
-			if (err)
-				res.send(err);
-			res.json(bear);
-		});
-	})
-
-	.put(function(req, res) {
-
-		// use our bear model to find the bear we want
-		Bear.findById(req.params.bear_id, function(err, bear) {
-
-			if (err)
-				res.send(err);
-
-			bear.name = req.body.name; 	// update the bears info
-
-			// save the bear
-			bear.save(function(err) {
-				if (err)
-					res.send(err);
-
-				res.json({ message: 'Bear updated!' });
-			});
-
-		});
-	})
-
-	.delete(function(req, res) {
-		Bear.remove({
-			_id: req.params.bear_id
-		}, function(err, bear) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Successfully deleted' });
-		});
-	});
 
 // Register Routes
 // all of our routes will be prefixed with /api
@@ -102,3 +41,67 @@ app.use('/api', router);
 // Server
 app.listen(port);
 console.log('Server listening on', port);
+
+// // API Routes, if we end up needing them
+// router.route('/tests')
+// 	// create a test at POST https://localhost:300/api/test
+// 	.post(function (req, res){
+
+// 		var test = new Test(); // create a new instance of test
+// 		test.name = req.body.name; // what is this test's name in the request?
+
+// 		test.save(function (err) { // yey 4 err hndlng doh
+// 			if (err) res.send(err);
+// 			res.json({message: 'Test created'});
+// 		});
+// 	})
+
+// 	.get(function (req, res) {
+// 		Test.find(function (err, tests) {
+// 			if (err) res.send(err);
+// 			res.json(tests)
+// 		})
+// 	});
+
+// router.route('/bears/:bear_id')
+
+// 	// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+// 	.get(function(req, res) {
+// 		Bear.findById(req.params.bear_id, function(err, bear) {
+// 			if (err)
+// 				res.send(err);
+// 			res.json(bear);
+// 		});
+// 	})
+
+// 	.put(function(req, res) {
+
+// 		// use our bear model to find the bear we want
+// 		Bear.findById(req.params.bear_id, function(err, bear) {
+
+// 			if (err)
+// 				res.send(err);
+
+// 			bear.name = req.body.name; 	// update the bears info
+
+// 			// save the bear
+// 			bear.save(function(err) {
+// 				if (err)
+// 					res.send(err);
+
+// 				res.json({ message: 'Bear updated!' });
+// 			});
+
+// 		});
+// 	})
+
+// 	.delete(function(req, res) {
+// 		Bear.remove({
+// 			_id: req.params.bear_id
+// 		}, function(err, bear) {
+// 			if (err)
+// 				res.send(err);
+
+// 			res.json({ message: 'Successfully deleted' });
+// 		});
+// 	});
