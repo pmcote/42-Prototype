acc032f70d5049798f10dcc95fa026e1bd7407cc
Rachel Mathew <rachelmathew999@gmail.com>
2014-12-07 13:22:10 -0500

    Run experiment updates and something else I think

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,9 @@
 $( document ).ready(function() {
-
-    
     $("#search").focus(function(){
     	$('#search-dropdown').removeClass('hide');
    	});
 
   	$("#hide-box").click(function(){
     	$('#search-dropdown').addClass('hide');
-   	}); 	
-
+   	}); 
 });
