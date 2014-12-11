949df4de7e847b5e9e622df8de7dd1b9d9f91e37
Rachel Mathew <rachelmathew999@gmail.com>
2014-12-11 01:05:05 -0500

    Experiment homepage finished

--- public/js/app.js
+++ public/js/app.js
@@ -6,4 +6,12 @@ $( document ).ready(function() {
   	$("#hide-box").click(function(){
     	$('#search-dropdown').addClass('hide');
    	}); 
+   	$("#delete_experiment").click(function(){
+    	document.body.style.backgroundColor="gray";
+   		$('#delete').removeClass('hide');
+   	}); 
+   	$('.cancel-button').click(function(){
+   		document.body.style.backgroundColor="white";
+   		$('#delete').addClass('hide');	
+   	});
 });
